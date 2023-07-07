3f9fe2c41dc5c4dbac19027cb21d83f48d66f384
gaurav <grathore07@gmail.com>
2023-07-08 03:33:00 +0530

    Add --noinput flag to migration commands in dockerfile

--- analytics/dockerfiles/scripts/entrypoint.sh
+++ analytics/dockerfiles/scripts/entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "Applying database migrations..."
 # TODO: Add --noinput flag to migration commands
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Generating swagger schema..."
 python manage.py spectacular --color --file schema.yml
