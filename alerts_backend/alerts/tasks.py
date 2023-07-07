64c200545d83a5b10b5c36fb063317d8834c45b7
gaurav <grathore07@gmail.com>
2023-07-08 03:36:30 +0530

    Add task to process event and send insights

--- alerts_backend/alerts/tasks.py
+++ alerts_backend/alerts/tasks.py
@@ -2,33 +2,12 @@ import logging
 
 from celery import shared_task
 from .utils import mails
-from ebay_sdk import client, utils as ebay_utils
-from pubsub import RedisProducer, Event
+from ebay_sdk import client
 
 from .models import Alert
-import time
-from django.conf import settings
+from .utils import pubsub as pubsub_utils
 
 logger = logging.getLogger(__name__)
-
-
-producer = RedisProducer(
-    channel=settings.PUBSUB_CHANNEL,
-    host=settings.PUBSUB_HOST,
-    port=settings.PUBSUB_PORT,
-    db=settings.PUBSUB_DEFAULT_DB
-)
-
-
-def publish_event(alert, response) -> None:
-    payload = {
-        'id': alert.id,
-        'timestamp': time.time(),
-        'keywords': alert.keywords,
-        'frequency': alert.frequency,
-        'response': response,
-    }
-    producer.produce(Event(type='products', payload=payload))
 
 
 @shared_task
@@ -37,17 +16,24 @@ def send_alert(frequency: int):
     logger.info(f"Found {len(alerts)} alerts for frequency {frequency}")
 
     for alert in alerts:
-        keywords = alert.keywords
-        response = client.BuyApi.find_items_by_keyword(keyword=keywords)
+        keywords: str = alert.keywords
+        response: dict = client.BuyApi.find_items_by_keyword(keyword=keywords)
 
-        logger.info('Publishing event...')
-        publish_event(alert, response)
-
-        items = ebay_utils.parse_response(response)
-        mails.send_html_mail(
-            to=alert.email,
-            subject=f'Alert for {keywords}',
-            context={'items': items, 'keywords': keywords},
-            template_name='mails/alert.html',
-        )
+        # items: list[ebay_models.ItemSummary] = ebay_utils.parse_response(response)
+        # mails.send_html_mail(
+        #     to=alert.email,
+        #     subject=f'Alert for {keywords}',
+        #     context={'items': items, 'keywords': keywords},
+        #     template_name='mails/alert.html',
+        # )
         logger.info(f"Sent email for alert - {alert}")
+
+        # Publish the details of newly fetched products
+        payload = {
+            'id': alert.id,
+            'email': alert.email,
+            'keywords': alert.keywords,
+            'frequency': alert.frequency,
+            'items': response
+        }
+        pubsub_utils.publish_event(type='alert.new_products', payload=payload)
