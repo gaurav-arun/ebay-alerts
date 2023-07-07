dfd456a59d0eea8a68a2d57e6644f5b2850e17a4
gaurav <grathore07@gmail.com>
2023-07-07 21:12:41 +0530

    Remove consume_event task as we are using management command to launch the pubsub consumer now.

--- analytics/insights/apps.py
+++ analytics/insights/apps.py
@@ -5,6 +5,6 @@ class InsightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'insights'
 
-    def ready(self):
-        from .tasks import consume_events
-        consume_events.apply_async()
+    # def ready(self):
+    #     from .tasks import consume_events
+    #     consume_events.apply_async()
