4fa44a73389f0978b7aa9b5171d65d3623bad350
gaurav <grathore07@gmail.com>
2023-07-07 12:56:36 +0530

    Start consume_event task as soon as the application starts

--- analytics/insights/apps.py
+++ analytics/insights/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class InsightsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'insights'
+
+    def ready(self):
+        from .tasks import consume_events
+        consume_events.apply_async()
