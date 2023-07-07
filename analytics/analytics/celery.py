f8c577c9cead63125bdc9c459f0a48519c2d8e3d
gaurav <grathore07@gmail.com>
2023-07-07 12:57:05 +0530

    Reduce the frequency to crontab

--- analytics/analytics/celery.py
+++ analytics/analytics/celery.py
@@ -2,23 +2,23 @@ import os  # isort:skip
 import logging  # isort:skip
 from django.conf import settings  # isort:skip
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics.settings_docker")  # isort:skip
+
 import celery
 from celery.schedules import crontab
 
 logger = logging.getLogger(__name__)
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics.settings")  # isort:skip
-
 app = celery.Celery("alerts_backend")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     "insights": {
         "task": "alerts.tasks.send_alert",
-        "schedule": crontab(minute="*/10"),
+        "schedule": crontab(hour="*/12"),
     },
     # "alert_every_10_minutes": {
     #     "task": "alerts.tasks.send_alert",
