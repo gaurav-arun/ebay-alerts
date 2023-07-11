68fbcfa02951c6b3590e73a5e53336ab814c744f
gaurav <grathore07@gmail.com>
2023-07-11 20:59:11 +0530

    Code cleanup in pubsub module

--- pubsub/producer.py
+++ pubsub/producer.py
@@ -3,11 +3,11 @@ import abc
 
 class Producer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def produce(self, *args, **kwargs) -> None:
+    def produce(self, *args, **kwargs):
         """Abstract method to handle incoming messages from the pub-sub system."""
         pass
 
     @abc.abstractmethod
-    def close(self, *args, **kwargs) -> None:
+    def close(self, *args, **kwargs):
         """Abstract method to close connections."""
         pass
