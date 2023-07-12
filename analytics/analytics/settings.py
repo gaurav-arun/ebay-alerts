2d49d5406c9df0a9e6d0ac0bf2a6ebb2ec613019
gaurav <grathore07@gmail.com>
2023-07-12 20:45:11 +0530

    Inject environment variables from docker-compose.yml
    - Removed dependency on dotenv module
    - Added .env.docker templates with reasonable defaults
    - Updated docker-compose.yml file
    - Added tabs for indentation

--- analytics/analytics/settings.py
+++ analytics/analytics/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import dotenv
-
-dotenv.load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
