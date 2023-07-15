e6006acd4f44584d34b01a3906086fc3dbfde1c2
gaurav <grathore07@gmail.com>
2023-07-15 22:27:45 +0530

    [pubsub] Restructure pubsub before converting into a dist package

--- pubsub/__init__.py
+++ pubsub/__init__.py
@@ -1,6 +1,6 @@
-from .pubsubevent import PubSubEvent, PubSubEventType
-from .redis_consumer import RedisConsumer
-from .redis_producer import RedisProducer
+from pubsub.pubsubevent import PubSubEvent, PubSubEventType
+from pubsub.redis_consumer import RedisConsumer
+from pubsub.redis_producer import RedisProducer
 
 __all__ = [
     "PubSubEvent",
