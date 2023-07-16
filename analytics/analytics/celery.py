c5fa061960e2723295213e33df9176a3df858476
gaurav <grathore07@gmail.com>
2023-07-16 20:19:54 +0530

    [analytics] Enable configuring product insight frequency from environment variable

--- analytics/analytics/celery.py
+++ analytics/analytics/celery.py
@@ -5,6 +5,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analytics.settings.base")  # is
 
 import celery  # noqa: E402
 from celery.schedules import crontab  # noqa: E402
+from django.conf import settings  # noqa: E402
 
 logger = logging.getLogger(__name__)
 
@@ -17,8 +18,9 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     "product_insights": {
         "task": "insights.tasks.send_product_insights",
-        # TODO: Change the crontab frequency to `day` after testing
-        "schedule": crontab(minute="*/30"),
+        "schedule": crontab(
+            minute=f"*/{settings.PRODUCT_INSIGHTS_FREQUENCY_IN_MINUTES}"
+        ),
         "kwargs": {"lookback_days": 14},
     },
 }
